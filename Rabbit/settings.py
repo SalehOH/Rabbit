6273d8a5a0cd96a3b0374ad48bee9ec3ccb5e0b8
Saleh <Salhussaini0@gmail.com>
2023-03-06 14:27:33 +0000

    INSTALLED_APPS  amended

--- Rabbit/settings.py
+++ Rabbit/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "authentication",
+    
+    "authentication.apps.AuthenticationConfig",
+    "RabbitHole.apps.RabbitholeConfig",
 ]
 
 MIDDLEWARE = [

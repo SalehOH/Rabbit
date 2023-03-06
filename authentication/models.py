290f6a809f1f88e23682cfd25cc220c5113b73ab
SalehOH <Salhussaini0@gmail.com>
2023-03-06 20:51:37 +0000

    Custom user model created and added to settings.py as main User model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+    name = None
+    email = models.EmailField(unique=True, null=False)
+    bio = models.CharField(max_length=250, null=True)
+
+    REQUIRED_FIELDS = ['email',]

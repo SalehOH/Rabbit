aa6cb6741dc61733af46287006a3ad88546ee835
Yicheng Wang <2721840w@student.gla.ac.uk>
2023-03-10 17:58:43 +0000

    First commit unit tests for views.py

--- RabbitHole/tests.py
+++ RabbitHole/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import Room, Post, Reply, Like
 
-# Create your tests here.
+User = get_user_model()
+
+
+
+class RoomViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser', email='test@example.com', password='testpass'
+        )
+        self.room = Room.objects.create(
+            name='testroom', avatar='room-avatar.jpg', creator=self.user
+        )
+        self.post = Post.objects.create(
+            content='test post content', room=self.room, user=self.user
+        )
+        self.reply = Reply.objects.create(
+            content='test reply content', post=self.post, user=self.user
+        )
+
+    def test_room_view(self):
+        response = self.client.get(reverse('room', args=[self.room.name]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'RabbitHole/room.html')
+        #self.assertContains(response, self.post.content)
+        #self.assertContains(response, self.reply.content)
+
+    def test_create_room_view(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('create_room'), {'name': 'newroom'})
+        self.assertEqual(response.status_code, 200)
+        #not sure?
+        self.assertFalse(Room.objects.filter(name='newroom').exists())
+
+    def test_create_post_view(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('create_post', args=[self.room.name]), {'content': 'test post content'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Post.objects.filter(content='test post content').exists())
+
+    def test_create_reply_view(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('create_reply', args=[self.room.name, self.post.id, self.post.slug]), {'content': 'test reply content'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Reply.objects.filter(content='test reply content').exists())
+
+
+
+
+
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.room = Room.objects.create(name='Test Room', avatar=SimpleUploadedFile("room_avatar.png", b"file_content"), creator=self.user)
+        self.post = Post.objects.create(content='Test Post', room=self.room, user=self.user)
+
+    def test_post_creation(self):
+        self.assertEqual(str(self.post), 'Test Post')
+        self.assertEqual(Post.objects.count(), 1)
+
+    def test_post_slug_creation(self):
+        self.assertEqual(self.post.slug, 'test-post')
+        self.assertEqual(Post.objects.get(pk=self.post.pk).slug, 'test-post')
+
+
+
+
+
+class ReplyTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.room = Room.objects.create(name='Test Room', avatar=SimpleUploadedFile("room_avatar.png", b"file_content"), creator=self.user)
+        self.post = Post.objects.create(content='Test Post', room=self.room, user=self.user)
+        self.reply = Reply.objects.create(content='Test Reply', post=self.post, user=self.user)
+
+    def test_reply_creation(self):
+        self.assertEqual(str(self.reply), 'Test Reply')
+        self.assertEqual(Reply.objects.count(), 1)
+
+    def test_reply_slug_creation(self):
+        self.assertEqual(self.reply.slug, 'test-reply')
+        self.assertEqual(Reply.objects.get(pk=self.reply.pk).slug, 'test-reply')

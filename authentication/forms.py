2f2228ac215ef7504190946961fbdd48e0e21e2c
SalehOH <Salhussaini0@gmail.com>
2023-03-20 03:48:54 +0000

    Login and signup page improved

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,16 +1,20 @@
 from allauth.account.forms import SignupForm, LoginForm
 from django.contrib.auth import get_user_model
+from django import forms
+from django.forms import FileInput
 
 User = get_user_model()
 
 class CustomSignupForm(SignupForm):
+    avatar = forms.ImageField(label='Avatar', required=False)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         attrs = {
             'username': ['Enter your username', 'form-control'],
             'email': ['Enter your email', 'form-control'],
             'password1': ['Enter your password', 'form-control'],
-            'password2': ['Confirm your password', 'form-control']
+            'password2': ['Confirm your password', 'form-control'],
+            'avatar': ['Select your avatar', 'form-control'],
         }
         for field_name, field in self.fields.items():
             widget = field.widget
@@ -18,6 +22,7 @@ class CustomSignupForm(SignupForm):
                 widget.attrs.update({'placeholder': attrs[field_name][0], 'class': attrs[field_name][1]})
             widget.attrs.update(attrs)
             widget.attrs.pop('autofocus', None)
+        
 
 class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
--- static/RabbitHole/js/room.js
+++ static/RabbitHole/js/room.js
@@ -2,18 +2,11 @@ const joinBtn = document.getElementById("join")
 const members = document.getElementById("members")
 const nav = document.getElementById("nav")
 
-const userEle = 
-`
-<li>
-    <a href="/u/${members.dataset.curuser}">${members.dataset.curuser}</a>
-</li>
-`
-const createPostEle = 
-`       
-  <a class="nav-link" href="/${joinBtn.dataset.roomname}/create-post/">Create a Post</a>
-        
-`
+let createPostEle = ``
 if(joinBtn){
+  createPostEle = 
+  `<a class="nav-link" href="/${joinBtn.dataset.roomname}/create-post/">Create a Post</a> `
+  
   joinBtn.addEventListener("click", ()=> {
         joinRoom(joinBtn.dataset.roomname, joinBtn.dataset.userid);
         nav.insertAdjacentHTML("afterbegin", createPostEle);
@@ -22,17 +15,39 @@ if(joinBtn){
 
 async function joinRoom(roomName, userId) {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/${roomName}/${userId}/join/`, {
+    const response = await fetch(`/${roomName}/${userId}/join/`, {
       method: "GET",
       headers: {"Content-Type": "application/json"},
     });
     if (response.ok) {
       joinBtn.remove();
-      members.insertAdjacentHTML("beforeend", userEle);
+      members.insertAdjacentHTML("beforeend", await get_user('admin'));
     } else {
         throw new Error("Error while joining the room");
     }
   } catch (error) {
     console.error(error);
   }
+}
+
+const get_user = async function(username){
+  console.log(username)
+  const response = await fetch(`/search/?q=${username}`);
+  if (response.ok){
+    const data = await response.json();
+    console.log(data)
+  }else{
+    throw new Error(" something went wrong")
+  }
+
+  const userEle = 
+    `
+    <li class="list-group-item">
+          <a class="nav-link" href="/u/${user.username}/">
+              <img class="room-list-image" src="/images/${room.avatar.replace(/\\/g, "/")}" alt="${user.username}">
+              <span class="mx-2">u/${user.username}</span>
+          </a>
+    </li>
+  `;
+  return userEle;
 }
